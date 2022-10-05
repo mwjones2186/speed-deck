60f2d85ccbdfbad1c95544cfbb816a38824b5ff7
kodapassey <kodapassey@charter.net>
2022-10-05 14:38:18 -0700

    working nav bar and beginning of login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
-// import React from "react";
-import { Navigation } from "./components/Nav";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Navigation } from "./components/Navigation";
+import { Home } from './components/Home';
+import { Leaderboard } from './components/Leaderboard';
+import { Profile } from './components/Profile';
+import { Progress } from './components/Progress';
+import { Login } from './components/Login-signup'
+import './css/app.css'
+
 
 function App() {
-    return (
-        <Navigation />
-    )
-}
+        return (
+            <>
+                <Navigation />
+
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/leaderboard" element={<Leaderboard />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/progress" element={<Progress />} />
+                    <Route path="/signup" element={<Login />} />
+                </Routes>
+            </>
+        )
+    }
 
 export default App;
