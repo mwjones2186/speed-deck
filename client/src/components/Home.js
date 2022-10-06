532cc048a3a0cf342836a1bf97ea6aeefcbc4cf7
Kdenn1 <103376676+Kdenn1@users.noreply.github.com>
2022-10-05 19:37:28 -0700

    main page stylings

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,13 @@
+
+import '../css/homepage.css';
+
 export const Home = () => {
     return (
+        <div>
             <h1>Home Page</h1>
+            <video autoplay muted loop id="myVideo">
+                <source src="./images/bigbrain.mp4" type="video/mp4"></source>
+            </video>
+        </div>
     )
 }
