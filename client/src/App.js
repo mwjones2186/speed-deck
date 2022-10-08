bb6b44788f16552e7af4c183277f016fb27d3c9a
Kdenn1 <103376676+Kdenn1@users.noreply.github.com>
2022-10-08 10:35:20 -0700

    added code that possibly works for generating a random addition, subtraction and multiplication problem, cannot test it as the site page does not load for me

--- client/src/App.js
+++ client/src/App.js
@@ -26,3 +26,30 @@ function App() {
     }
 
 export default App;
+
+// random addition question code 
+function question() {
+    this.a = Math.round(Math.random()*10);
+    this.b = Math.round(Math.random()*10);
+    this.result = this.a + this.b;
+    this.checkResult = function(givenResultString) {
+        return (""+result == givenResultString);
+    }
+}
+
+// random addition, multiplication, and subtraction code 
+function question() {
+    var add(x, y) { return x+y; };
+    var subtract(x, y) { return x-y; };
+    var multiply(x, y) { return x*y };
+    var operators = [add, subtract, multiply];
+
+    this.a = Math.round(Math.random()*10);
+    this.b = Math.round(Math.random()*10);
+    var operatorIdx = Math.min(Math.floor(Math.random()*4),3);
+    this.operator = operators[operatorIdx];
+    this.result = operator(this.a,this.b);
+    this.checkResult = function(givenResultString) {
+        return (""+this.result == givenResultString);
+    }
+}
