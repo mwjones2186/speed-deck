35fded11b2468e59f0b871cf592ea1f0ce4ce106
Royal Jam <103871471+Jamsniffer@users.noreply.github.com>
2022-10-10 16:37:35 -0700

    added validations to new user sign up form

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import '../css/navigation.css';
 
 
-export default function Navigation  ()  {
+function UserLoggedIn(props) {
     return (
         <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
             <Navbar.Toggle aria-controls='navbarScroll' data-bs-target='#navbarScroll' />
@@ -16,7 +16,26 @@ export default function Navigation  ()  {
                         <NavLink className='navItem' href='/'>Home</NavLink>
                         <NavLink className='navItem' href='/profile'>Profile</NavLink>
                         <NavLink className='navItem' href='/leaderboard'>Leaderboard</NavLink>
-                        <NavLink className='navItem' href='/levelSelect'>Level Select</NavLink>
+                        <NavLink className='navItem' href='/progress'>Progress</NavLink>
+                        <NavLink className='navItem login' href='/signup'>Logout</NavLink>
+                    </Nav>
+                </div>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+function UserLoggedOut(props) {
+    return (
+        <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+            <Navbar.Toggle aria-controls='navbarScroll' data-bs-target='#navbarScroll' />
+            <Navbar.Collapse id='navbarScroll'>
+                <Link to='/' className='h1Wrap'>
+                    <h1 className='navh1'>SpeedDeck</h1>
+                </Link>
+                <div className='wrap'>
+                    <Nav className='navWrap'>
+                        <NavLink className='navItem' href='/'>Home</NavLink>
                         <NavLink className='navItem login' href='/signup'>Login/SignUp</NavLink>
                     </Nav>
                 </div>
@@ -24,3 +43,14 @@ export default function Navigation  ()  {
         </Navbar>
     )
 }
+
+
+
+export default function Navigation(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+        return <UserLoggedIn />;
+    }
+    return <UserLoggedOut />;
+}
+
--- client/src/pages/Login-signup.js
+++ client/src/pages/Login-signup.js
@@ -1,31 +1,115 @@
 import { Container, Row, Col } from "react-bootstrap";
 import '../css/login-signup.css';
 
-export default function Login (){
+var signUpUsernameValid, signUpPasswordValid, signUpEmailValid;
+export default function Login() {
     return (
         <Container className="pageWrap">
             <Row>
                 <Col lg={6} className="login">
                     <h1>Login to Your Account</h1>
                     <form className="form">
-                        <input type='text' placeholder="Email" id="email" />
-                        <input type='text' placeholder="Password" id="password" />
+                        <input type='text' placeholder="Email" id="loginEmail" />
+                        <input type='text' placeholder="Password" id="loginPassword" />
                         <button className="signInBtn" type="submit">Sign In</button>
                     </form>
                 </Col>
                 <Col lg={6} className="signup">
                     <h1>New Here?</h1>
                     <p>Sign up and have fun playing all of our math related games!</p>
-                    <form>
+                    <form name='signUp' onSubmit={handleSubmit}>
                         <input type='text' placeholder="Username" id="username" />
-                        <input type='text' placeholder="Email" id="email" />
-                        <input type='text' placeholder="Password" id="password" />
-                        <input type='text' placeholder="Re-EnterPassword" id="password" />
+                        <input type='text' placeholder="Email" id="email" name='email' />
+                        <input type='password' placeholder="Password" id="password" />
+                        <input type='password' placeholder="Re-EnterPassword" id="rePassword" />
+                        <button className="signUpBtn" type="submit">Sign up</button>
                     </form>
-                    <button>Sign up</button>
                 </Col>
-               
+
             </Row>
         </Container>
     )
+}
+const handleSubmit = event => {
+    event.preventDefault();
+    console.log('form submitted')
+    let form = document.getElementsByName("signUp")[0];
+    let username = document.getElementById('username').value;
+    let maxChar = 20
+    let minChar = 3
+
+    if (username.length >= minChar && username.length <= maxChar) {
+        console.log('username correct length')
+        signUpUsernameValid = true;
+    } else {
+        signUpUsernameValid = false;
+        console.log('username is not correct length')
+
+    }
+
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(document.getElementById('email').value)) {
+        console.log('email validation worked')
+        signUpEmailValid = true;
+        //      return (true)
+    } else {
+        signUpEmailValid = false;
+
+        console.log("You have entered an invalid email address!")
+        //        return (false)
+    }
+
+    // password is invalid, email is invalid
+    // email is not valid
+    confirmPassword();
+
+    let successMsg = document.getElementById("successMsg");
+    let newMsg = "";
+
+    if (successMsg == null || successMsg == undefined) {
+        successMsg = document.createElement("span");
+        successMsg.id = "successMsg";
+        if (!signUpUsernameValid) {
+            successMsg.innerHTML += "Username is invalid";
+        }
+        if (!signUpEmailValid) {
+            if (successMsg.innerHTML.length > 0) successMsg.innerHTML += ", ";
+            successMsg.innerHTML += "Email is invalid";
+        }
+        if (!signUpPasswordValid) {
+            if (successMsg.innerHTML.length > 0) successMsg.innerHTML += ", ";
+            successMsg.innerHTML += "Password must be 6-20 characters long";
+        }
+        form.before(successMsg);
+    }
+    else {
+        successMsg.innerHTML = "";
+        if (!signUpUsernameValid) {
+            successMsg.innerHTML += "Username is invalid";
+        }
+        if (!signUpEmailValid) {
+            if (successMsg.innerHTML.length > 0) successMsg.innerHTML += ", ";
+            successMsg.innerHTML += "Email is invalid";
+        }
+        if (!signUpPasswordValid) {
+            if (successMsg.innerHTML.length > 0) successMsg.innerHTML += ", ";
+            successMsg.innerHTML += "Password must be 6-20 characters long";
+        }
+
+    }
+
+}
+
+function confirmPassword() {
+    let password1 = document.getElementById('password').value;
+    let password2 = document.getElementById('rePassword').value;
+    let minChar = 6;
+    let maxChar = 20;
+    console.log(password1 + " " + password2 + " " + password1.length + "  " + password2.length);
+    if (password1 === password2 && (password1.length >= minChar && password1.length <= maxChar)) {
+        console.log('success');
+        signUpPasswordValid = true;
+    } else {
+        console.log('error');
+        signUpPasswordValid = false;
+    }
 }
