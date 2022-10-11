97e8b27b6d515ee0c2131783d8bcdd8807bd9b02
Kdenn1 <103376676+Kdenn1@users.noreply.github.com>
2022-10-10 18:30:04 -0700

    added full leaderboard html and css for leaderboard page

--- client/src/Pages/Leaderboard.js
+++ client/src/Pages/Leaderboard.js
@@ -1,5 +1,46 @@
+import '../css/Leaderboard.css';
+
 export const Leaderboard = () => {
     return (
-            <h1>Leaderboard Page</h1>
+            <><h1></h1><div className="Leaderboard">
+                <header>
+                    <h1>Leaderboard</h1>
+                <nav>
+                    <a href="">Solo</a>
+                    <a href="">Versus</a>
+                    <a href="">Battle Royale</a>
+                </nav>
+                </header>
+                <table>
+                    <thead>
+                        <tr>
+                        <th className="Rank">Rank</th>
+                        <th className="Name">Name</th>
+                        <th className="Country">Country</th>
+                        <th className="Score">Time</th>
+                        </tr>
+                    </thead>
+                <tbody>
+                    <tr>
+                        <td className="Rank">1</td>
+                        <td className="Name">Placeholder</td>
+                        <td className="Country">USA</td>
+                        <td className="Score">12.2</td>
+                    </tr>
+                    <tr>
+                        <td className="Rank">2</td>
+                        <td className="Name">Placeholder</td>
+                        <td className="Country">USA</td>
+                        <td className="Score">12.3</td>
+                    </tr>
+                    <tr>
+                        <td className="Rank">3</td>
+                        <td className="Name">Placeholder</td>
+                        <td className="Country">USA</td>
+                        <td className="Score">12.4</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div></>
     )
 }
