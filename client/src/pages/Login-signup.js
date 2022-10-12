c12f6677f910df99f86ff4dcfe699bad0bb2cbfe
Royal Jam <103871471+Jamsniffer@users.noreply.github.com>
2022-10-11 19:59:24 -0700

    updated login form

--- client/src/pages/Login-signup.js
+++ client/src/pages/Login-signup.js
@@ -6,105 +6,134 @@ import Auth from "../utils/auth";
 import { ADD_USER, LOGIN } from "../utils/mutations";
 
 export default function Login(props) {
-  const [formState, setFormState] = useState({
-    email: "",
-    password: "",
-    username: "",
-  });
-  const [addUser] = useMutation(ADD_USER);
-  const [login] = useMutation(LOGIN);
-
-  const handleFormSubmitSignup = async (event) => {
-    event.preventDefault();
-    const form = event.currentTarget;
-
-    if(form.checkValidity() === false){
-      event.preventDefault()
-      event.stopPropagation()
-    }
-
-    try {
-      console.log(formState);
-      const { data } = await addUser({
-        variables: {
-          ...formState,
-        },
-      });
-
-      Auth.login(data.addUser.token);
-    } catch (err) {
-      console.error(err);
-    }
-
-    setFormState({
-      email: "",
-      password: "",
-      username: "",
-    })
-  };
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormState({
-      ...formState,
-      [name]: value,
+    const [formState, setFormState] = useState({
+        email: "",
+        password: "",
+        username: "",
     });
-  };
+    const [addUser] = useMutation(ADD_USER);
+    const [login] = useMutation(LOGIN);
 
-  const handleFormSubmitLogin = async (event) => {
-    event.preventDefault();
+    const handleFormSubmitSignup = async (event) => {
+        event.preventDefault();
+        const form = event.currentTarget;
 
-    const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
 
-    if(form.checkValidity() === false){
-      event.preventDefault()
-      event.stopPropagation()
-    }
-    
-    try {
-      
-      const { data } = await login({
-        variables: {
-          ...formState,
-        },
-      });
+        try {
+            console.log(formState);
+            const { data } = await addUser({
+                variables: {
+                    ...formState,
+                },
+            });
 
-      Auth.login(data.login.token);
-    } catch (err) {
-      console.error(err);
-    }
+            Auth.login(data.addUser.token);
+        } catch (err) {
+            console.error(err);
+        }
 
-    setFormState({
-      email: "",
-      password: "",
-    })
-  };
+        setFormState({
+            email: "",
+            password: "",
+            username: "",
+        })
+    };
 
-export default function Login (){
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmitLogin = async (event) => {
+        event.preventDefault();
+
+        const form = event.currentTarget;
+
+        if (form.checkValidity() === false) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+
+        try {
+
+            const { data } = await login({
+                variables: {
+                    ...formState,
+                },
+            });
+
+            Auth.login(data.login.token);
+        } catch (err) {
+            console.error(err);
+        }
+
+        setFormState({
+            email: "",
+            password: "",
+        })
+    };
+
     return (
         <Container className="pageWrap">
-            <Row>
-                <Col lg={6} className="login">
-                    <h1>Login to Your Account</h1>
-                    <form className="form">
-                        <input type='text' placeholder="Email" id="email" />
-                        <input type='text' placeholder="Password" id="password" />
-                        <button className="signInBtn" type="submit">Sign In</button>
-                    </form>
-                </Col>
-                <Col lg={6} className="signup">
-                    <h1>New Here?</h1>
-                    <p>Sign up and have fun playing all of our math related games!</p>
-                    <form>
-                        <input type='text' placeholder="Username" id="username" />
-                        <input type='text' placeholder="Email" id="email" />
-                        <input type='text' placeholder="Password" id="password" />
-                        <input type='text' placeholder="Re-EnterPassword" id="password" />
-                    </form>
-                    <button>Sign up</button>
-                </Col>
-               
-            </Row>
-        </Container>
+      <Row>
+        <Col  className="login">
+          <h1>Login to Your Account</h1>
+          <form onSubmit={handleFormSubmitLogin} className="form">
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formState.email}
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              name="password"
+              value={formState.password}
+              onChange={handleChange}
+              placeholder="Password"
+            />
+            <button className="signInBtn" type="submit">
+              Sign In
+            </button>
+          </form>
+        </Col>
+        <Col  className="signup">
+          <h1>New Here?</h1>
+          <p>Sign up and have fun playing all of our math related games!</p>
+          <form onSubmit={handleFormSubmitSignup}>
+            <input
+              type="text"
+              placeholder="Username"
+              value={formState.username}
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formState.email}
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              name="password"
+              value={formState.password}
+              onChange={handleChange}
+              placeholder="Password"
+            />
+          <button type="submit">Sign up</button>
+          </form>
+        </Col>
+      </Row>
+    </Container>
     )
 }
